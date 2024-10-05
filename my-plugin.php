b7fe3902d46936224f437d3b5eaf26588a1515dd
Ali Hasan <alihasandeveloper@gmail.com>
2024-10-05 17:20:11 +0600

    php file created

---
+++ my-plugin.php
@@ -0,0 +1,16 @@
+<?php 
+
+/*
+ * Plugin Name: My Plugin
+ * Description: My plugin for testing
+ * Version: 1.0.0
+ * Author: Ali Hasan
+ * Author URI: https://github.com/alihasandeveloper
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Requires at least: 5.2
+ * Tested up to: 6.2
+ * Requires PHP: 7.2
+ * Text Domain: my-plugin
+ * Domain Path: /languages
+ */
